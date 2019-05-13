10b9604ad0e0c9c8d8761985f59982c63ab0de85
MJ <mj@mag.cr>
2019-05-13 23:53:33 +0200

    updates

---
+++ main.js
@@ -0,0 +1,41 @@
+const add = document.querySelector('.plus');
+let field = document.querySelector('.bottom');
+let textarea = document.querySelector('.textarea');
+let addBtn = document.querySelector('.convert');
+let newDo;
+
+
+add.addEventListener('click', function(){
+    textarea.style.display = 'block';
+    addBtn.style.display = 'flex';
+    add.style.display = 'none';
+});
+
+addBtn.addEventListener('click', function(){
+    newDo = textarea.value;
+    textarea.value = '';
+    textarea.style.display = 'none';
+    addBtn.style.display = 'none';
+    add.style.display = 'flex';
+    create();
+});
+
+function create(){
+    let newSection = document.createElement('div');
+        newSection.classList = 'item';
+    let para = document.createElement('p');
+        para.classList = 'item-txt';
+        para.contentEditable = true;
+        para.innerHTML = newDo;
+    let rmv = document.createElement('div');
+        rmv.classList = 'item-dlt';
+        rmv.innerHTML = 'x';
+    newSection.appendChild(para);
+    newSection.appendChild(rmv);
+    field.appendChild(newSection);
+    console.log(newSection);
+}
+
+function remove(){
+    
+}
