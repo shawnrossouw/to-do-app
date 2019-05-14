fa28f9c5abb93ab820feab6d7688ea599662bd87
MJ <mj@mag.cr>
2019-05-14 23:37:33 +0200

    working javascript

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const add = document.querySelector('.plus');
 let field = document.querySelector('.bottom');
+let theField = document.querySelector('.bottom') + 1;
 let textarea = document.querySelector('.textarea');
 let addBtn = document.querySelector('.convert');
 let newDo;
+let list = [];
 
 
 add.addEventListener('click', function(){
@@ -23,19 +25,28 @@ addBtn.addEventListener('click', function(){
 function create(){
     let newSection = document.createElement('div');
         newSection.classList = 'item';
+
     let para = document.createElement('p');
         para.classList = 'item-txt';
         para.contentEditable = true;
         para.innerHTML = newDo;
+
     let rmv = document.createElement('div');
         rmv.classList = 'item-dlt';
         rmv.innerHTML = 'x';
+        rmv.addEventListener('click', function(){
+            this.parentElement.parentElement.removeChild(this.parentElement);
+            var indexNr = list.indexOf(this.parentElement);
+            list.splice(indexNr,1);
+            console.log(list);
+        });
+
     newSection.appendChild(para);
     newSection.appendChild(rmv);
     field.appendChild(newSection);
-    console.log(newSection);
-}
 
-function remove(){
+    list.push(newSection);
+
+    console.log(list);
     
 }
